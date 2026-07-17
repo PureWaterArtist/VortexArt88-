be270cdfded1db21b6b55092debbf5fd2ed13014
PureWaterArtist <vortexart88@gmail.com>
2026-07-16 20:45:10 -0400

    Add lidinoid engine to simulation components

--- core_engines/run_all_simulations.py
+++ core_engines/run_all_simulations.py
@@ -72,6 +72,7 @@ def main():
         "components/gyroid-flow-regulator/gyroid_engine.py",
         "components/costa-surface-regulator/costa_engine.py",
         "components/chen-gackstatter-regulator/chen_engine.py",
+        "components/lidinoid-flow-regulator/lidinoid_engine.py",
         "components/flower-of-life-mesh/generate_fol_mesh.py",
         "components/cross-cap-regulator/cross_cap_engine.py",
         "components/outer-pressure-casing/casing_vectors.py",
