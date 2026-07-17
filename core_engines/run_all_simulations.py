d6cd461a08aa989013c256b747ccc46b15de860c
PureWaterArtist <vortexart88@gmail.com>
2026-07-16 22:54:27 -0400

    Add barth_engine.py to simulation components

--- core_engines/run_all_simulations.py
+++ core_engines/run_all_simulations.py
@@ -80,6 +80,7 @@ def main():
         "components/120-cell-hyper-manifold/hyper_manifold_engine.py",
         "components/600-cell-hyper-manifold/hyper_icosahedral_engine.py",
         "components/grand-antiprism-regulator/antiprism_engine.py",
+        "components/barth-sextic-regulator/barth_engine.py",
         "components/flower-of-life-mesh/generate_fol_mesh.py",
         "components/cross-cap-regulator/cross_cap_engine.py",
         "components/outer-pressure-casing/casing_vectors.py",
