997e6f4ff2b8fd62c98ff38ccc98c3e2f7cbbe5d
PureWaterArtist <vortexart88@gmail.com>
2026-07-16 23:05:40 -0400

    Add soliton_engine to simulation components list

--- core_engines/run_all_simulations.py
+++ core_engines/run_all_simulations.py
@@ -81,6 +81,7 @@ def main():
         "components/600-cell-hyper-manifold/hyper_icosahedral_engine.py",
         "components/grand-antiprism-regulator/antiprism_engine.py",
         "components/barth-sextic-regulator/barth_engine.py",
+        "components/sine-gordon-soliton-regulator/soliton_engine.py",
         "components/flower-of-life-mesh/generate_fol_mesh.py",
         "components/cross-cap-regulator/cross_cap_engine.py",
         "components/outer-pressure-casing/casing_vectors.py",
