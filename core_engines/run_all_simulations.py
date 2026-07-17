c522aad890e5017f9a0c24083bcc340b67b240ee
PureWaterArtist <vortexart88@gmail.com>
2026-07-17 00:58:32 -0400

    Add lax_pair_engine.py to simulation components

--- core_engines/run_all_simulations.py
+++ core_engines/run_all_simulations.py
@@ -88,6 +88,7 @@ def main():
         "components/kuznetsov-ma-soliton-regulator/kuznetsov_ma_engine.py",
         "components/e8-hyper-manifold/e8_manifold_engine.py",
         "components/kp-resonant-regulator/kp_engine.py",
+        "components/lax-pair-regulator/lax_pair_engine.py",
         "components/flower-of-life-mesh/generate_fol_mesh.py",
         "components/cross-cap-regulator/cross_cap_engine.py",
         "components/outer-pressure-casing/casing_vectors.py",
