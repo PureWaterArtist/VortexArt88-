4e9cf25bf6108e986d6312cd47e818eddae075a3
PureWaterArtist <vortexart88@gmail.com>
2026-07-17 00:29:52 -0400

    Add e8_manifold_engine.py to simulation components

--- core_engines/run_all_simulations.py
+++ core_engines/run_all_simulations.py
@@ -86,6 +86,7 @@ def main():
         "components/breather-soliton-regulator/breather_engine.py",
         "components/peregrine-soliton-regulator/peregrine_engine.py",
         "components/kuznetsov-ma-soliton-regulator/kuznetsov_ma_engine.py",
+        "components/e8-hyper-manifold/e8_manifold_engine.py",
         "components/flower-of-life-mesh/generate_fol_mesh.py",
         "components/cross-cap-regulator/cross_cap_engine.py",
         "components/outer-pressure-casing/casing_vectors.py",
